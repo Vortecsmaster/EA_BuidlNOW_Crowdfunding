be27d6227af41e113424f8aa81a182990bc3bbd5
Vortecsmaster <vortecsmaster@vortecsla.com>
2024-11-10 01:31:32 -0500

    Lucid Evolution code
    Configuraiton

--- crowdfunding_v01/off-chain/ts-test/crowdfunding_v01.ts
+++ crowdfunding_v01/off-chain/ts-test/crowdfunding_v01.ts
@@ -1,8 +1,12 @@
-import { Lucid, Blockfrost, SpendingValidator, fromHex, toHex, Credential, credentialToAddress } from "https://deno.land/x/lucid@0.10.7/mod.ts";
-import * as cbor from "https://deno.land/x/cbor@v1.4.1/index.js";
-import test01_signingKey from "../../../Wallets/Adr11skey.json" with {type: 'json'};
+import {Blockfrost, Lucid,LucidEvolution, SpendingValidator, fromHex, toHex, Credential, credentialToAddress } from "npm:@lucid-evolution/lucid";
+//import * as cbor from "https://deno.land/x/cbor@v1.4.1/index.js";
+import test01_signingKey from "/home/cardano/Dev/Wallets/Adr11skey.json" with {type: 'json'};
 
-const lucid = await Lucid.new(new Blockfrost("https://cardano-preview.blockfrost.io/api/v0", Deno.env.get("BLOCKFROST_PROJECT_ID"),),"Preview");
+const project_path = "/home/cardano/Dev/CREOW/crowdfunding_v02";
+
+const network = "Preview";
+
+const lucid = await Lucid(new Blockfrost("https://cardano-preview.blockfrost.io/api/v0", Deno.env.get("BLOCKFROST_PROJECT_ID"),),network);
 
 const paymentCredential: Credential = {
     type: "Key",
@@ -13,8 +17,9 @@ const stakeCredential: Credential = {
       type: "Key",
       hash: "8662fe85a22159022d71aebcf4342bcafaa6ede0df2e486a4e751e8e" // taken from cardano-cli generated stake verification key hash
   };
-const address11 = lucid.utils.credentialToAddress(paymentCredential, stakeCredential);
-await Deno.writeTextFile("our.addr", address11);
+const address11 = credentialToAddress(network,paymentCredential, stakeCredential);
+console.log("Address11:" + address11);
+//await Deno.writeTextFile("/home/cardano/Dev/Wallets/Adr11.addr", address11);
 
 // Alternative, generate new private keys with Lucid
 //  const ourPrivateKey = lucid.utils.generatePrivateKey();
