74e9ee778da067cce5774fa354881d8f00c83431
Vortecsmaster <vortecsmaster@vortecsla.com>
2024-11-11 00:12:33 -0500

    Updating off-chain code
    Updating off-chain code

--- crowdfunding_v01/off-chain/ts-test/crowdfunding_v01.ts
+++ crowdfunding_v01/off-chain/ts-test/crowdfunding_v01.ts
@@ -1,13 +1,14 @@
-import {Blockfrost, Lucid,LucidEvolution, SpendingValidator, fromHex, Credential, credentialToAddress, applyDoubleCborEncoding } from "npm:@lucid-evolution/lucid";
+import {Blockfrost, Lucid,LucidEvolution, SpendingValidator, fromHex, Credential, credentialToAddress, applyDoubleCborEncoding, validatorToAddress } from "npm:@lucid-evolution/lucid";
 //import * as cbor from "https://deno.land/x/cbor@v1.4.1/index.js";
 //import test01_signingKey from "/home/cardano/Dev/Wallets/Adr11skey.json" with {type: 'json'};
 import {network} from "./settings.ts"
 
 const project_path = "/home/cardano/Dev/CREOW/crowdfunding_v02";
 
-// const network = "Preview";
-
 const lucid = await Lucid(new Blockfrost("https://cardano-preview.blockfrost.io/api/v0", Deno.env.get("BLOCKFROST_PROJECT_ID"),),network);
+
+
+// Construct address from credentials and import keys.
 
 const paymentCredential: Credential = {
     type: "Key",
@@ -18,21 +19,9 @@ const stakeCredential: Credential = {
       type: "Key",
       hash: "8662fe85a22159022d71aebcf4342bcafaa6ede0df2e486a4e751e8e" // taken from cardano-cli generated stake verification key hash
   };
-const address11 = credentialToAddress(network,paymentCredential, stakeCredential);
-console.log("Address11:  " + address11);
+const bob_address = credentialToAddress(network, paymentCredential, stakeCredential);
+console.log("Address11:  " + bob_address);
 
-
-//await Deno.writeTextFile("/home/cardano/Dev/Wallets/Adr11.addr", address11);
-// Alternative, generate new private keys with Lucid
-//  const ourPrivateKey = lucid.utils.generatePrivateKey();
-//  await Deno.writeTextFile("OurAddress.skey", ourPrivateKey);
-//Cardano Private key in bech32; not the BIP32 private key or any key that is not fully derived. 
-// * Only an Enteprise address (without stake credential) is derived.
-
-// const address11 = await lucid
-//    .selectWalletFromPrivateKey(test01_signingKey.ed25519)  
-//    .credentialToAddress(paymentCredential, stakeCredential)
-//    .wallet.address();
 
 //read validator from blueprint json file created with aiken
 
@@ -46,4 +35,27 @@ console.log("Address11:  " + address11);
   };
 }
 
-console.log(validator);
+const campaignsAddress = validatorToAddress(network, validator,); 
+
+
+console.log(campaignsAddress);
+
+// Create a campaign
+// const campaign = async (): Promise<Result<TxComplete>> => { 
+//   try {
+  
+//     if (!lucid) throw "Uninitialized Lucid";
+//     if (!bob_address) throw "Non defnied address";
+    
+//     const tx = await lucid
+//       .newTx()
+//       .payToContract(<validator_addr>,<inline: datum>,<assets>)
+
+//     console.log("TODO BIEN!")
+//   }
+//   catch (error) {
+//     console.log("Error: " + error);
+//     if (error instanceof Error) return { type: "error", error: error };
+//     return { type: "error", error: new Error(`${JSON.stringify(error)}`) };
+//   }
+// }
